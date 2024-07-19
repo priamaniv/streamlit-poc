4a31598a8f4715211b126921927005f8003b6d81
pria maniv <priya.manivannan@convergetp.com>
2024-07-19 09:43:00 -0400

    updated logging

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def reset_game():
     st.session_state.index = 0
     st.session_state.letters_and_colors = LETTERS_AND_COLORS
     if st.session_state.name:
-        streamlit_root_logger.info(f"{st.session_state.name} Team: {st.session_state.team} started game, their word is {st.session_state.target}")
+        streamlit_root_logger.info(f"Name: {st.session_state.name} Team: {st.session_state.team} started game, their word is {st.session_state.target}")
 
 
 def go_back():
